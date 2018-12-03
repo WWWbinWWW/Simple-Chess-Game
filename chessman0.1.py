9edd9b29158efb52321b875a60cb3d56c53e238b
Code_W <395485299@qq.com>
2018-12-03 10:41:15 +0800

    update
    Add been eaten

--- chessman0.1.py
+++ chessman0.1.py
@@ -69,6 +69,7 @@ class chessmen(object):
 			tempx, tempy = item.curx, item.cury
 			chess_map.state[tempx, tempy] = 'B'
 
+		#判断是否能吃对手棋子
 		#Top Direction
 		if self.curx - 1 >= 0:
 			if chess_map.state[self.curx-1][self.cury] == self.onwer:
@@ -77,7 +78,9 @@ class chessmen(object):
 						print("You can eat the chess {} form {}".format((self.curx-2,self.cury), temp))
 						#根据被吃棋子的坐标找到该棋子，life置为0，并从棋盘中移除
 						tempx, tempy = self.curx-2, self.cury
+						#遍历对手场上所有棋子
 						for item in chess_map.get_owner(temp):
+							#将对应位置棋子置0
 							if item.curx == tempx and item.cury == tempy:
 								item.life = 0
 								chess_map.get_owner(temp).remove(item)
@@ -180,6 +183,56 @@ class chessmen(object):
 						#chess_map.get_owner(temp).remove((self.curx,self.cury-1))
 						chess_map.state[self.curx][self.cury-1] = '0.0'
 						return chess_map
+		#判断是否自杀
+		#上方向
+		if self.curx - 1 >= 0:
+			if self.curx - 2 >= 0:
+				if chess_map.state[self.curx-1][self.cury] == temp and chess_map.state[self.curx-2][self.cury] == temp:
+					print("Your chess {}{} is eaten!".format(self.onwer,(self.curx,self.cury)))
+					for item in chess_map.get_owner(self.onwer):
+							if item.curx == self.curx and item.cury == self.cury:
+								item.life = 0
+								chess_map.state[self.curx][self.cury] = '0.0'
+								chess_map.get_owner(self.onwer).remove(item)
+						#chess_map.get_owner(temp).remove((self.curx+2,self.cury))
+					return chess_map
+		#下方向
+		if self.curx + 1 < chess_map.height:
+			if self.curx + 2 < chess_map.height:
+				if chess_map.state[self.curx+1][self.cury] == temp and chess_map.state[self.curx+2][self.cury] == temp:
+					print("Your chess {}{} is eaten!".format(self.onwer,(self.curx,self.cury)))
+					for item in chess_map.get_owner(self.onwer):
+							if item.curx == self.curx and item.cury == self.cury:
+								item.life = 0
+								chess_map.state[self.curx][self.cury] = '0.0'
+								chess_map.get_owner(self.onwer).remove(item)
+						#chess_map.get_owner(temp).remove((self.curx+2,self.cury))			
+					return chess_map
+		#左方向
+		if self.cury - 1 >= 0:
+			if self.cury - 2 >= 0:
+				if chess_map.state[self.curx][self.cury-1] == temp and chess_map.state[self.curx][self.cury-2] == temp:
+					print("Your chess {}{} is eaten!".format(self.onwer,(self.curx,self.cury)))
+					for item in chess_map.get_owner(self.onwer):
+							if item.curx == self.curx and item.cury == self.cury:
+								item.life = 0
+								chess_map.state[self.curx][self.cury] = '0.0'
+								chess_map.get_owner(self.onwer).remove(item)
+						#chess_map.get_owner(temp).remove((self.curx+2,self.cury))
+					return chess_map
+
+		if self.cury + 1 < chess_map.width:
+			if self.cury + 2 < chess_map.width:
+				if chess_map.state[self.curx][self.cury+1] == temp and chess_map.state[self.curx][self.cury+2] == temp:
+					print("Your chess {}{} is eaten!".format(self.onwer,(self.curx,self.cury)))
+					for item in chess_map.get_owner(self.onwer):
+							if item.curx == self.curx and item.cury == self.cury:
+								item.life = 0
+								chess_map.state[self.curx][self.cury] = '0.0'
+								chess_map.get_owner(self.onwer).remove(item)
+						#chess_map.get_owner(temp).remove((self.curx+2,self.cury))
+					return chess_map
+
 		return chess_map
 
 
@@ -360,68 +413,72 @@ class ChessPlate(object):
 		print("Please input right number!")
 		return False
 
+def game_init():
+	Player = 1
+	Num = 1
+	Dir = 1
+	chessA1 = chessmen(0,0,0,0,'A','A1',1)
+	chessA2 = chessmen(1,0,1,0,'A','A2',1)
+	chessA3 = chessmen(2,0,2,0,'A','A3',1)
+	chessA4 = chessmen(3,0,3,0,'A','A4',1)
+	chessA5 = chessmen(4,0,4,0,'A','A5',1)
+	chessB1 = chessmen(0,6,0,6,'B','B1',1)
+	chessB2 = chessmen(1,6,1,6,'B','B2',1)
+	chessB3 = chessmen(2,6,2,6,'B','B3',1)
+	chessB4 = chessmen(3,6,3,6,'B','B4',1)
+	chessB5 = chessmen(4,6,4,6,'B','B5',1)
+	PlayerA = [chessA1, chessA2, chessA3, chessA4, chessA5]
+	PlayerB = [chessB1, chessB2, chessB3, chessB4, chessB5]
 
-Player = 1
-Num = 1
-Dir = 1
-chessA1 = chessmen(0,0,0,0,'A','A1',1)
-chessA2 = chessmen(1,0,1,0,'A','A2',1)
-chessA3 = chessmen(2,0,2,0,'A','A3',1)
-chessA4 = chessmen(3,0,3,0,'A','A4',1)
-chessA5 = chessmen(4,0,4,0,'A','A5',1)
-chessB1 = chessmen(0,6,0,6,'B','B1',1)
-chessB2 = chessmen(1,6,1,6,'B','B2',1)
-chessB3 = chessmen(2,6,2,6,'B','B3',1)
-chessB4 = chessmen(3,6,3,6,'B','B4',1)
-chessB5 = chessmen(4,6,4,6,'B','B5',1)
-PlayerA = [chessA1, chessA2, chessA3, chessA4, chessA5]
-PlayerB = [chessB1, chessB2, chessB3, chessB4, chessB5]
+	Chess_map = ChessPlate(WIDTH, HEIGHT, PlayerA, PlayerB)
+	Chess_map.game_begin()
+	print(Chess_map.showmap())
+	ChessA_map = {'1':chessA1,
+				'2':chessA2,
+				'3':chessA3,
+				'4':chessA4,
+					'5':chessA5}
+	ChessB_map = {'1':chessB1,
+				'2':chessB2,
+				'3':chessB3,
+				'4':chessB4,
+				'5':chessB5}
 
-Chess_map = ChessPlate(WIDTH, HEIGHT, PlayerA, PlayerB)
-Chess_map.game_begin()
-print(Chess_map.showmap())
+	while Chess_map.is_end() == False:
 
-ChessA_map = {'1':chessA1,
-			'2':chessA2,
-			'3':chessA3,
-			'4':chessA4,
-			'5':chessA5}
-ChessB_map = {'1':chessB1,
-			'2':chessB2,
-			'3':chessB3,
-			'4':chessB4,
-			'5':chessB5}
-
-while Chess_map.is_end() == False:
-
-	if Player == 1:
-		print("PLayerA's time!")
-		Num = input("Please input number of chessA:")
-		while Chess_map.is_alive(Player, Num) == False:
+		if Player == 1:
+			print("PLayerA's time!")
 			Num = input("Please input number of chessA:")
-		Dir = input("Please input the direction w,a,s,d:")
-		while Dir not in ['w','a','s','d']:
-			Dir = input("Please choose one form w,a,s,d:")
-		Chess_map, flat = ChessA_map[Num].chess_move(Chess_map, Direction[Dir])
-		while flat != 1:
-			Dir = input("Please choose the right direction again:")
+			while Chess_map.is_alive(Player, Num) == False:
+				Num = input("Please input number of chessA:")
+			Dir = input("Please input the direction w,a,s,d:")
+			while Dir not in ['w','a','s','d']:
+				Dir = input("Please choose one form w,a,s,d:")
 			Chess_map, flat = ChessA_map[Num].chess_move(Chess_map, Direction[Dir])
-		Player = 0
+			while flat != 1:
+				Dir = input("Please choose the right direction again:")
+				Chess_map, flat = ChessA_map[Num].chess_move(Chess_map, Direction[Dir])
+			Player = 0
 
-	if Player == 0:
-		print("PLayerB's time!")
-		Num = input("Please input number of chessB:")
-		while Chess_map.is_alive(Player, Num) == False:
+		if Player == 0:
+			print("PLayerB's time!")
 			Num = input("Please input number of chessB:")
-		Dir = input("Please input the direction w,a,s,d:")
-		while Dir not in ['w','a','s','d']:
-			Dir = input("Please choose one form w,a,s,d:")
-		Chess_map, flat = ChessB_map[Num].chess_move(Chess_map, Direction[Dir])
-		while flat != 1:
-			Dir = input("Please choose the right direction again:")
+			while Chess_map.is_alive(Player, Num) == False:
+				Num = input("Please input number of chessB:")
+			Dir = input("Please input the direction w,a,s,d:")
+			while Dir not in ['w','a','s','d']:
+				Dir = input("Please choose one form w,a,s,d:")
 			Chess_map, flat = ChessB_map[Num].chess_move(Chess_map, Direction[Dir])
-		Player = 1
+			while flat != 1:
+				Dir = input("Please choose the right direction again:")
+				Chess_map, flat = ChessB_map[Num].chess_move(Chess_map, Direction[Dir])
+			Player = 1
 
+def main(argv=None):
+	game_init()
+
+if __name__ == '__main__':
+	main()
 
 #MapStates = StateTree()
 #MapStates.add(MapStates.root,Chess_map.state)
